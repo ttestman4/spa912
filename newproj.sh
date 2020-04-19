467a4ca953f236ef64ce294f45695ddd85c64eea
Test Man <ttestman4@gmail.com>
2020-04-19 07:12:18 +0530

    Ass lib 2

--- newproj.sh
+++ newproj.sh
@@ -1,3 +1,4 @@
 # foo/node_modules/.bin/ng new spa912 --create-application="false" --directory spa912  -p spa --routing --skip-install --strict --style scss -v
 # yarn ng generate application app1 -p app1 --routing --skip-install --style scss 
-yarn ng generate library lib1 -p lib1 --skip-install --lint-fix
+# yarn ng generate library lib1 -p lib1 --skip-install --lint-fix
+# yarn ng generate library lib2 -p lib2 --skip-install --lint-fix
