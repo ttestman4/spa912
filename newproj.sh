5e69f2e7578ee11ecb34426c10533afaced1ad88
Test Man <ttestman4@gmail.com>
2020-04-19 07:05:38 +0530

    Lib 1 added

--- newproj.sh
+++ newproj.sh
@@ -1,2 +1,3 @@
 # foo/node_modules/.bin/ng new spa912 --create-application="false" --directory spa912  -p spa --routing --skip-install --strict --style scss -v
-yarn ng generate application app1 -p app1 --routing --skip-install --style scss 
+# yarn ng generate application app1 -p app1 --routing --skip-install --style scss 
+yarn ng generate library lib1 -p lib1 --skip-install --lint-fix
---
+++ projects/lib1/src/lib/lib1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib1-lib1',
+  template: `
+    <p>
+      lib1 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Lib1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
