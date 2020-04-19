f7de279e9807bbdbf6bf5d404c93c04bb9cd2104
Test Man <ttestman4@gmail.com>
2020-04-19 06:58:52 +0530

    Empty spa app

---
+++ newproj.sh
@@ -0,0 +1,2 @@
+# foo/node_modules/.bin/ng new spa912 --create-application="false" --directory spa912  -p spa --routing --skip-install --strict --style scss -v
+yarn ng generate application app1 -p app1 --routing --skip-install --style scss 
